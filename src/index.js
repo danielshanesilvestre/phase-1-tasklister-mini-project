225f247d6fa5455879e464ee17417748cf5d297e
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-06-26 12:39:55 -0500

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.getElementById("create-task-form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let input = document.getElementById("new-task-description").value;
+        let list = document.getElementById("list");
+
+        let li = document.createElement("li");
+        li.textContent = input + " ";
+        list.appendChild(li);
+
+        let li_button = document.createElement("button");
+        li_button.setAttribute("data-description", input);
+        li_button.textContent = "X";
+        li.appendChild(li_button);
+
+        li_button.addEventListener("click", function(event) {
+            event.target.parentNode.remove();
+        });
+    });
 });
